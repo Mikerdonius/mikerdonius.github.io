eef8b17d425dc407c8f84135c62d7edecd34352d
Mikerdonius <mikedegrootjest@gmail.com>
2022-01-28 11:34:45 +0100

    end

--- js/script.js
+++ js/script.js
@@ -9,22 +9,22 @@
 
 
 
-(function(){
-    // Slide In Panel - by CodyHouse.co
+(function () {
+	// Slide In Panel - by CodyHouse.co
 	var panelTriggers = document.getElementsByClassName('js-cd-panel-trigger');
-	if( panelTriggers.length > 0 ) {
-		for(var i = 0; i < panelTriggers.length; i++) {
-			(function(i){
-				var panelClass = 'js-cd-panel-'+panelTriggers[i].getAttribute('data-panel'),
+	if (panelTriggers.length > 0) {
+		for (var i = 0; i < panelTriggers.length; i++) {
+			(function (i) {
+				var panelClass = 'js-cd-panel-' + panelTriggers[i].getAttribute('data-panel'),
 					panel = document.getElementsByClassName(panelClass)[0];
 				// open panel when clicking on trigger btn
-				panelTriggers[i].addEventListener('click', function(event){
+				panelTriggers[i].addEventListener('click', function (event) {
 					event.preventDefault();
 					addClass(panel, 'cd-panel--is-visible');
 				});
 				//close panel when clicking on 'x' or outside the panel
-				panel.addEventListener('click', function(event){
-					if( hasClass(event.target, 'js-cd-close') || hasClass(event.target, panelClass)) {
+				panel.addEventListener('click', function (event) {
+					if (hasClass(event.target, 'js-cd-close') || hasClass(event.target, panelClass)) {
 						event.preventDefault();
 						removeClass(panel, 'cd-panel--is-visible');
 					}
@@ -32,23 +32,23 @@
 			})(i);
 		}
 	}
-	
+
 	//class manipulations - needed if classList is not supported
 	//https://jaketrent.com/post/addremove-classes-raw-javascript/
 	function hasClass(el, className) {
-	  	if (el.classList) return el.classList.contains(className);
-	  	else return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
+		if (el.classList) return el.classList.contains(className);
+		else return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
 	}
 	function addClass(el, className) {
-	 	if (el.classList) el.classList.add(className);
-	 	else if (!hasClass(el, className)) el.className += " " + className;
+		if (el.classList) el.classList.add(className);
+		else if (!hasClass(el, className)) el.className += " " + className;
 	}
 	function removeClass(el, className) {
-	  	if (el.classList) el.classList.remove(className);
-	  	else if (hasClass(el, className)) {
-	    	var reg = new RegExp('(\\s|^)' + className + '(\\s|$)');
-	    	el.className=el.className.replace(reg, ' ');
-	  	}
+		if (el.classList) el.classList.remove(className);
+		else if (hasClass(el, className)) {
+			var reg = new RegExp('(\\s|^)' + className + '(\\s|$)');
+			el.className = el.className.replace(reg, ' ');
+		}
 	}
 })();
 
@@ -79,7 +79,7 @@ var modals = document.getElementsByClassName('modal');
 
 // Get the button that opens the modal
 var btns = document.getElementsByClassName("openmodal");
-var spans=document.getElementsByClassName("close");
+var spans = document.getElementsByClassName("close");
 
 // for(let i=0;i<btns.length;i++){
 //     btns[i].onclick = function() {
@@ -87,18 +87,18 @@ var spans=document.getElementsByClassName("close");
 //     }
 // }
 
-for(let i=0;i<spans.length;i++){
-    spans[i].onclick = function() {
-        modals[i].style.display = "none";
-    }
+for (let i = 0; i < spans.length; i++) {
+	spans[i].onclick = function () {
+		modals[i].style.display = "none";
+	}
 }
 
-window.onclick = function(event) {
-    if (event.target.classList.contains('modal')) {
-     for (var i in modals) {
-      if (typeof modals[i].style !== 'undefined') modals[i].style.display = "none";    
-     }
-    }
+window.onclick = function (event) {
+	if (event.target.classList.contains('modal')) {
+		for (var i in modals) {
+			if (typeof modals[i].style !== 'undefined') modals[i].style.display = "none";
+		}
+	}
 }
 
 
@@ -106,26 +106,24 @@ window.onclick = function(event) {
 
 
 
-	document.getElementById('addButton1').addEventListener('click', () =>{
-		var newdiv = document.createElement('div')
-		newdiv.classList.add('myBtn100')
-		newdiv.classList.add('bugfix')
-		var buttontext = prompt('Fill in text')
-		newdiv.textContent = buttontext
-		document.getElementById('grid-item2').append(newdiv)
-	})
-	
-
-	document.getElementById('addButton2').addEventListener('click', () =>{
-		var newdiv = document.createElement('div')
-		newdiv.classList.add('myBtn101')
-		newdiv.classList.add('bugfix')
-		var buttontext = prompt('Fill in text')
-		newdiv.textContent = buttontext
-		document.getElementById('grid-item2').append(newdiv)
-	})	
+document.getElementById('addButton1').addEventListener('click', () => {
+	var newdiv = document.createElement('div')
+	newdiv.classList.add('myBtn100')
+	newdiv.classList.add('bugfix')
+	var buttontext = prompt('Fill in text')
+	newdiv.textContent = buttontext
+	document.getElementById('grid-item2').append(newdiv)
+})
 
 
+document.getElementById('addButton2').addEventListener('click', () => {
+	var newdiv = document.createElement('div')
+	newdiv.classList.add('myBtn101')
+	newdiv.classList.add('bugfix')
+	var buttontext = prompt('Fill in text')
+	newdiv.textContent = buttontext
+	document.getElementById('grid-item2').append(newdiv)
+})
 
 
 
@@ -133,70 +131,3 @@ window.onclick = function(event) {
 
 
 
-	const canvasEl = document.querySelector('#canvas');
-
-const w = canvasEl.width = window.innerWidth;
-const h = canvasEl.height = window.innerHeight * 2;
-
-function loop() {
-  requestAnimationFrame(loop);
-	ctx.clearRect(0,0,w,h);
-  
-  confs.forEach((conf) => {
-    conf.update();
-    conf.draw();
-  })
-}
-
-function Confetti () {
-  //construct confetti
-  const colours = ['#fde132', '#009bde', '#ff6b00'];
-  
-  this.x = Math.round(Math.random() * w);
-  this.y = Math.round(Math.random() * h)-(h/2);
-  this.rotation = Math.random()*360;
-
-  const size = Math.random()*(w/60);
-  this.size = size < 15 ? 15 : size;
-
-  this.color = colours[Math.floor(colours.length * Math.random())];
-
-  this.speed = this.size/7;
-  
-  this.opacity = Math.random();
-
-  this.shiftDirection = Math.random() > 0.5 ? 1 : -1;
-}
-
-Confetti.prototype.border = function() {
-  if (this.y >= h) {
-    this.y = h;
-  }
-}
-
-Confetti.prototype.update = function() {
-  this.y += this.speed;
-  
-  if (this.y <= h) {
-    this.x += this.shiftDirection/3;
-    this.rotation += this.shiftDirection*this.speed/100;
-  }
-
-  if (this.y > h) this.border();
-};
-
-Confetti.prototype.draw = function() {
-  ctx.beginPath();
-  ctx.arc(this.x, this.y, this.size, this.rotation, this.rotation+(Math.PI/2));
-  ctx.lineTo(this.x, this.y);
-  ctx.closePath();
-  ctx.globalAlpha = this.opacity;
-  ctx.fillStyle = this.color;
-  ctx.fill();
-};
-
-const ctx = canvasEl.getContext('2d');
-const confNum = Math.floor(w / 4);
-const confs = new Array(confNum).fill().map(_ => new Confetti());
-
-loop();
